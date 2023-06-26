0a55f7e11c7b6e651395f2c4df76752f9e6b7d0c
Mayank-B01 <np03cs4s230205@heraldcollege.edu.np>
2023-06-26 21:30:02 +0545

    completed the js files for the workshop and deleted the testing files

--- Week 3/script.js
+++ Week 3/script.js
@@ -9,17 +9,13 @@ let buttons = document.querySelectorAll ("button");
 function calcResult(move1, move2){
     if (move1 == move2){
         statusDisplay.textContent = tieMsg;
-        moveDisplays[0].textContent = "You played" + moveList[move1];
-        moveDisplays[1].textContent = "Computer played" + moveList[move2];
+        return {"Message": tieMsg, "player":move1, "Computer":move2}
     }
-    else if ((move1 == 0 && move2 == 1) || (move1 == 1 && move2 == 2)){
-        statusDisplay.textContent = winMsg;
-        moveDisplays[0].textContent = "You played" + moveList[move1];
-        moveDisplays[1].textContent = "Computer played" + moveList[move2];
+    else if ((move1 == 0 && move2 == 2) || (move1 == 1 && move2 == 0) || (move1 == 2 && move2 == 1)){
+        return { "Message": winMsg, "player":move1, "Computer":move2}
     }
-    else if (move1 == 1 && move2 == 2){
-        statusDisplay.textContent = winMsg;
-
+    else{
+        return{ "Message": loseMsg, "player":move1, "Computer":move2} 
     }
 }
 function randomMove(){
@@ -29,11 +25,31 @@ function randomMove(){
 function startGame(){
     statusDisplay.textContent = 'Choose !'
     for (i=0; i < buttons.length; i++ ){
+        buttons[i].removeEventListener("click",startGame);
+        buttons[i].addEventListener("click",endGame);
         buttons[i].textContent = moveList[i];
-        buttons[i].style.display = "inline-block"
+        buttons[i].style.visibility = "visible";
+        buttons[i].style.display = "inline-block";
+    }
+    for (i=0; i <moveDisplays.length; i++){
+        moveDisplays[i].style.visibility = 'hidden';
     }
 }
 function endGame(event){
-
+    let player_move = moveList.indexOf(event.target.innerHTML);
+    let computer_move = randomMove();
+    let movements = calcResult(player_move, computer_move);
+    document.getElementById("status-head").innerHTML = movements["Message"];
+    for (i = 0; i <buttons.length; i = i + 2 ){
+        buttons[i].style.visibility = 'hidden';
+    }
+    document.querySelectorAll("button")[1].innerHTML = "Play Again";
+    buttons[1].addEventListener("click", startGame);
+    for(i = 0; i < moveDisplays.length; i++){
+        moveDisplays[i].style = 'display:visible';
+    }
+    
+    moveDisplays[0].innerHTML = "You played" + " " +moveList[player_move];
+    moveDisplays[1].innerHTML = "Computer played" +  " " +moveList[computer_move];
 }
 startGame();
